866c06a5741053ca2e3de81d8b86471fce623fe9
lazygunner <gymgunner@gmail.com>
2014-07-28 22:56:00 +0800

    finished all show spider

--- yyets/yyets/pipelines.py
+++ yyets/yyets/pipelines.py
@@ -7,6 +7,7 @@
 import sys
 import pymysql
 import hashlib
+from datetime import datetime
 
 from scrapy.http import Request
 from spiders.episodes_spider import EpisodesSpider
@@ -22,35 +23,24 @@ class MySQLStorePipeLine(object):
         self.items = []
 
     def process_item(self, item, spider):
-        try:
-            test = item['show_id']
-            if test == None:
-                return item
-        except:
-            return item
+        #try:
+        #    test = item['show_id']
+        #    if test == None:
+        #        return item
+        #except:
+        #    return item
         self.items.append(item)
     def open_spider(self, spider):
-        #self.episode_count = self.cursor.execute("""SELECT COUNT(*) FROM episodes WHERE show_id = %s""", spider.show_id)
-        #print 'hehe'
-        #print self.episode_count
         pass
 
-    def _get_new_episodes(self, show_id):
+    def __get_new_episodes(self, show_id):
         rows = self.cursor.execute("""SELECT 'index', 'ed2k_link' FROM episodes WHERE show_id = %s""", show_id)
         old_episodes = self.cursor.fetchall()
         old_epi_dict = {}
         items = []
-        #import pdb
-        #pdb.set_trace()
         for i in old_episodes:
             old_epi_dict[i[0]] = i[1]
         for item in self.items:
-            #item['format'] = item['format'].encode('utf-8')
-            #item['show_id'] = item['show_id'].encode('utf-8')
-            #item['index'] = item['index'].encode('utf-8')
-            #item['ed2k_link'] = item['ed2k_link'].encode('utf-8')
-            #item['season'] = str(item['season'])
-            #item['episode'] = str(item['episode'])
             e_index = item['e_index']
 
             if e_index not in old_epi_dict or item['ed2k_link'] == old_epi_dict[e_index]:
@@ -58,22 +48,36 @@ class MySQLStorePipeLine(object):
                 items.append(item_tuple)
         return items
 
-
-    def close_spider(self, spider):
-
-        if spider.__class__  != EpisodesSpider:
-            return
-
-        new_items = self._get_new_episodes(spider.show_id)
+    def _handle_episodes(self, show_id):
+        new_items = self.__get_new_episodes(show_id)
 
         try:
-            #self.cursor.executemany("""INSERT INTO episodes (show_id, format, season, episode, ed2k_link) VALUES (%s, %s, %s, %s, %s)""", (item['show_id'].encode('utf-8'), item['format'].encode('utf-8'), str(item['season']), str(item['episode']), item['ed2k_link'].encode('utf-8')))
-            print new_items[0]
             self.cursor.executemany("""REPLACE INTO episodes (e_index, show_id, format, season, episode, ed2k_link) VALUES (%s, %s, %s, %s, %s, %s)""", new_items)
             self.conn.commit()
-
-
         except pymysql.Error, e:
             print "Error %d: %s" % (e.args[0], e.args[1])
 
-#        return item
+    def _handle_all_show(self):
+        shows = []
+        date = datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S")
+
+        for show in self.items:
+            show_tuple = (show['show_id'], show['show_name'], date, date, 0, 0)
+            shows.append(show_tuple)
+
+        try:
+            self.cursor.executemany("""REPLACE INTO shows (show_id, show_name, created_time, updated_time, latest_season, latest_episode) \
+                    VALUES (%s, %s, %s, %s, %s, %s)""", shows)
+            self.conn.commit()
+        except pymysql.Error, e:
+            print "Error %d: %s" % (e.args[0], e.args[1])
+
+
+    def close_spider(self, spider):
+
+        if spider.name  == 'episodes':
+            self._handle_episodes(spider.show_id)
+        elif spider.name == 'all_show':
+            self._handle_all_show()
+
+
