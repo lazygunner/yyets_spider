ac59e8ed7dc212efceecf4a1a960cf843e9fc576
lazygunner <gymgunner@gmail.com>
2014-10-23 17:28:53 +0800

    record crontab

--- scrapy_cron.sh
+++ scrapy_cron.sh
@@ -1,1 +1,3 @@
-source /alidata1/workspace/yyets/bin/activatei && cd /alidata1/workspace/yyets/yyetsi && scrapy crawl update_today
+source /alidata1/workspace/yyets/bin/activate && cd /alidata1/workspace/yyets/yyetsi && scrapy crawl update_today
+0 */2 * * *     gunner . /alidata1/workspace/yyets/bin/activate && cd /alidata1/workspace/yyets/yyets && scrapy crawl update_today
+0  0    1 * *   gunner . /alidata1/workspace/yyets/bin/activate && cd /alidata1/workspace/yyets/yyets && scrapy crawl all_show
